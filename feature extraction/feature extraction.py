2b8cde50246219ed44310dcb5308771240e40e1e
xinshuaiiii <xinshuai0819@163.com>
2025-04-22 21:42:21 +0800

    Delete feature extraction directory

--- feature extraction/feature extraction.py
+++
@@ -1,6 +0,0 @@
-from prott5 import ProteinEmbeddingExtractor
-from vit import ViTFeatureExtractorModel
-extractor_seq = ProteinEmbeddingExtractor()
-extractor_seq.process_file("train_name_URL.csv","seq_train_embeddings.npy","seq_train_attention_masks.npy")
-extractor_img = ViTFeatureExtractorModel()
-extractor_img.extract_features_for_all( "train"  ,"train_vitembeddings.npy")
